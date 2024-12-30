43b7a61ce9d651e79ff485d533cb01ef8c5837ea
권상훈 <tkd1886@hanmail.net>
2024-12-30 22:19:05 +0900

    24/12/30
    애셋 추가

---
+++ Assets/Script/Door.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Door : InteractiableObject
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
